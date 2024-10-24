1e865aafdd7f13384f8e7665261cdc767696d94f
Dante <2802471732@qq.com>
2024-10-24 20:46:32 +0800

    koa ctx介绍

--- Nodejs工程师养成计划/第9章 KOA重构项目/koa-test/app.js
+++ Nodejs工程师养成计划/第9章 KOA重构项目/koa-test/app.js
@@ -2,7 +2,13 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
+    // koa把req和res封装成了ctx
     ctx.body = 'Hello World';
+    
+    console.log(ctx.req.method);
+    console.log(ctx.req.url);
+    console.log(ctx.req.headers);
+    
 });
 
 app.listen(3000, () => {
